d69d58c5c548c25375f112bdf1603e86c03ae1d7
Emanuelle Vodiani <118612780+ManuVodi@users.noreply.github.com>
2025-01-16 15:16:38 -0300

    CREATE basis project
    Created files for the project and them respectives folders

---
+++ src/shared/config/type-orm.config.ts
@@ -0,0 +1,18 @@
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+export const DB_DATABASE = process.env.DB_DATABASE;
+
+export const typeOrmConfig: TypeOrmModuleOptions = {
+  type: 'postgres',
+  name: process.env.DB_DATABASE,
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT),
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  schema: process.env.DB_SCHEMA,
+  entities: [__dirname + './../**/*.entity.{js,ts}'],
+  synchronize: false,
+};
