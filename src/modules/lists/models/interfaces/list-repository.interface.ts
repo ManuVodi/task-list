5c639a16b286ecf6559e09d883260737e788f729
Emanuelle Vodiani <118612780+ManuVodi@users.noreply.github.com>
2025-01-20 17:36:41 -0300

    UPDATE task and list (interface)
    Changed variables in interface files

--- src/modules/lists/models/interfaces/list-repository.interface.ts
+++ src/modules/lists/models/interfaces/list-repository.interface.ts
@@ -1,10 +1,11 @@
 import { CreateListDTO } from "src/shared/dtos/create-list.dto";
-import { DeleteResult, InsertResult } from "typeorm";
+import { DeleteResult, InsertResult, UpdateResult } from "typeorm";
 import { ListEntity } from "../entities/list.entity";
+import { UpdateListDTO } from "src/shared/dtos/update-list.dto";
 
 export interface IListRepository {
     create(list: CreateListDTO): Promise<InsertResult>;
-    update(id: number): Promise<InsertResult>;
-    find_all(id?: number): ListEntity[];
-    delete(id: number): Promise<DeleteResult>;
+    update(id_list: number, list: UpdateListDTO): Promise<UpdateResult>;
+    find(id_list?: number): ListEntity[];
+    delete(id_list: number): Promise<DeleteResult>;
 }
--- src/modules/tasks/models/interfaces/task-repository.interface.ts
+++ src/modules/tasks/models/interfaces/task-repository.interface.ts
@@ -1,11 +1,11 @@
 import { TaskEntity } from "src/modules/tasks/models/entities/task.entity";
 import { CreateTaskDTO } from "src/shared/dtos/create-task.dto";
 import { UpdateTaskDTO } from "src/shared/dtos/update-task.dto";
-import { DeleteResult, InsertResult } from "typeorm";
+import { DeleteResult, InsertResult, UpdateResult } from "typeorm";
 
 export interface ITaskRepository {
     create(task: CreateTaskDTO): Promise<InsertResult>;
-    update(id: number, task: UpdateTaskDTO): Promise<InsertResult>;
-    find_all(id: number): Promise<TaskEntity[]>;
-    delete(id: number): Promise<DeleteResult>;
+    update(id_task: number, task: UpdateTaskDTO): Promise<UpdateResult>;
+    find(id_task: number): Promise<TaskEntity[]>;
+    delete(id_task: number): Promise<UpdateResult>;
 }
