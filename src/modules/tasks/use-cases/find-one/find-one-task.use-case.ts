db708b829e0f8a404d4d8c59418e411395f6b86a
Emanuelle Vodiani <118612780+ManuVodi@users.noreply.github.com>
2025-01-29 17:55:14 -0300

    FEAT task (find-one)
    Added http requests errors messages

--- src/modules/tasks/use-cases/find-one/find-one-task.use-case.ts
+++ src/modules/tasks/use-cases/find-one/find-one-task.use-case.ts
@@ -1,18 +1,34 @@
-import { Inject, Injectable, InternalServerErrorException } from "@nestjs/common";
+import { BadRequestException, HttpException, Inject, Injectable, InternalServerErrorException, NotAcceptableException, NotFoundException, UnprocessableEntityException } from "@nestjs/common";
 import { ITaskRepository } from "../../models/interfaces/task-repository.interface";
 
 @Injectable()
 export class FindOneTaskUseCase {
     @Inject('ITaskRepository')
-    private readonly taskRepository: ITaskRepository;
+    private readonly taskRepository: ITaskRepository
 
     async execute(id_task: number){
         try {
+            if (isNaN(id_task)) {
+                throw new NotAcceptableException({message: 'Parametro id da task tem que ser um numero'})
+            }
             const task = await this.taskRepository.find_one(id_task)
+
+            if (!task) {
+                throw new NotFoundException({message: 'Task não encontrada'})
+            }
+            
             return task;
         }
         catch(error){
-            throw new InternalServerErrorException('Falha ao buscar a tarefa')
+            if (error instanceof NotFoundException) {
+                throw new NotFoundException(error.message)
+            }
+
+            if (error instanceof NotAcceptableException) {
+                throw new NotAcceptableException(error.message)
+            }
+            
+            throw new InternalServerErrorException({message: "Falha ao deletar tarefa"})
         }
     }
 }
