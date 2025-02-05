10f96b96b215f1baac8354d2c922c0e164c8132b
Emanuelle Vodiani <118612780+ManuVodi@users.noreply.github.com>
2025-02-05 16:50:12 -0300

    FEAT list (module)
    Added all use cases and controllers in list module

--- src/modules/lists/list.module.ts
+++ src/modules/lists/list.module.ts
@@ -1,9 +1,44 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DB_DATABASE } from 'src/shared/config/type-orm.config';
+import { ListEntity } from './models/entities/list.entity';
+import { CreateListController } from './use-cases/create/create-list.controller';
+import { UpdateListController } from './use-cases/update/update-list.controller';
+import { FindOneListController } from './use-cases/find-one/find-one-list.controller';
+import { FindAllListsController } from './use-cases/find-all/find-all-list.controller';
+import { DeleteListController } from './use-cases/delete/delete-list.controller';
+import { CreateListUseCase } from './use-cases/create/create-list.use-case';
+import { UpdateListUseCase } from './use-cases/update/update-list.use-case';
+import { FindOneListUseCase } from './use-cases/find-one/find-one-list.use-case';
+import { FindAllListsUseCase } from './use-cases/find-all/find-all-list.use-case';
+import { DeleteListUseCase } from './use-cases/delete/delete-list.use-case';
+import { ListTypeOrmRepository } from './repositories/list.repository';
 
 @Module({
-  imports: [],
-  controllers: [],
-  providers: [],
-  exports: [],
+  imports: [
+    TypeOrmModule.forFeature([ListEntity], DB_DATABASE)
+  ],
+  controllers: [
+    CreateListController,
+    UpdateListController,
+    FindOneListController,
+    FindAllListsController,
+    DeleteListController
+  ],
+  providers: [
+    CreateListUseCase,
+    UpdateListUseCase,
+    FindOneListUseCase,
+    FindAllListsUseCase,
+    DeleteListUseCase,
+    ListTypeOrmRepository,
+    {
+      provide: 'IListRepository',
+      useExisting: ListTypeOrmRepository
+    }
+  ],
+  exports: [
+    FindOneListUseCase
+  ],
 })
 export class ListModule {}
